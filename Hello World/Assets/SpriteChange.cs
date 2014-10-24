40a30574dab99b1afa8c97a77092b323eb12238e
Andrewl140106 <andrewl140106@gmail.com>
2014-10-23 21:54:06 -0700

    Hello world
    Hello World completed.

---
+++ Hello World/Assets/SpriteChange.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteChange : MonoBehaviour {
+
+	public Sprite sp1;
+	public Sprite sp2;
+	private SpriteRenderer srr;
+
+	void Start () {
+		srr = gameObject.GetComponent<SpriteRenderer> ();
+	}
+	
+	void Update () {
+		if (Input.GetMouseButtonDown (0)
+		    || Input.GetMouseButtonDown (1)) {
+			if (srr.sprite == sp1) 
+				srr.sprite = sp2;
+			else if (srr.sprite == sp2) 
+				srr.sprite = sp1;
+		}
+	}
+}
---
+++ Hello World/Assets/SpriteMove.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteMove : MonoBehaviour {
+
+	public float moveSpeed = 0.05f;
+
+	void Start () {
+		//
+	}
+	
+	void Update () {
+		Vector3 tempPosition = transform.position;
+		if (Input.GetAxis ("Horizontal") > 0) 
+			tempPosition.x += moveSpeed;
+		if (Input.GetAxis ("Horizontal") < 0)
+			tempPosition.x -= moveSpeed;
+		if (Input.GetAxis ("Vertical") > 0) 
+			tempPosition.y += moveSpeed;
+		if (Input.GetAxis ("Vertical") < 0)
+			tempPosition.y -= moveSpeed;
+
+		transform.position = tempPosition; 
+	}
+}
