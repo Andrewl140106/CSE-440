4c1255026d8bd049cd8d19252f4442b166f667f5
Andrewl140106 <andrewl140106@gmail.com>
2014-10-31 11:18:15 -0700

    Creating Game World
    Still missing a jump function.

---
+++ Game World/Assets/PlayerAnimator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimator : MonoBehaviour {
+
+	public Sprite[] sprites;
+	public float framesPerSecond;
+	private SpriteRenderer sr;
+
+	void Start () {
+		sr = renderer as SpriteRenderer;
+	}
+	
+	void Update () {
+		if (Input.GetAxis ("Horizontal") > 0
+		    || Input.GetAxis ("Horizontal") < 0
+		    || Input.GetAxis ("Vertical") > 0
+		    || Input.GetAxis ("Vertical") < 0) {
+			int i = (int)(Time.timeSinceLevelLoad * framesPerSecond);
+			i = i % sprites.Length;
+			sr.sprite = sprites [i];
+		}
+	}
+}
---
+++ Game World/Assets/PlayerMoveControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMoveControl : MonoBehaviour {
+
+	public float moveSpeed;
+
+	void Start () {
+	//
+	}
+	
+	void Update () {
+		Vector3 tempPosition = transform.position;
+		if (Input.GetAxis ("Horizontal") > 0)
+			tempPosition.x += moveSpeed;
+		if (Input.GetAxis ("Horizontal") < 0)
+			tempPosition.x -= moveSpeed;
+		if (Input.GetAxis ("Vertical") > 0)
+			tempPosition.y += moveSpeed;
+		if (Input.GetAxis ("Vertical") < 0)
+			tempPosition.y -= moveSpeed;
+
+		transform.position = tempPosition;
+	}
+}
