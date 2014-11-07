00dbd8299d610c09c70ca5078d275fffc8b69da2
Andrewl140106 <andrewl140106@gmail.com>
2014-11-06 18:38:16 -0800

    Physics

---
+++ Physics/Assets/PlayerMoveControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMoveControl : MonoBehaviour {
+
+	public float moveSpeed;
+	public float jumpSpeed;
+
+	private Rigidbody2D rbody;
+
+	void Start () {
+		rbody = gameObject.GetComponent<Rigidbody2D> ();
+	}
+	
+	void Update () {
+		Vector3 tempPosition = transform.position;
+		if (Input.GetAxis ("Horizontal") > 0)
+			tempPosition.x += moveSpeed;
+		if (Input.GetAxis ("Horizontal") < 0)
+			tempPosition.x -= moveSpeed;
+	
+		transform.position = tempPosition;
+	}
+
+	void FixedUpdate () {
+		if (Input.GetKeyDown (KeyCode.Space)) 
+			rbody.AddForce (Vector2.up * jumpSpeed);
+	}
+}
